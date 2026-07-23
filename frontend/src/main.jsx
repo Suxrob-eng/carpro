5ed6d752ff2e516e196af22a036fb22a118a8ec4
Suxrob <shuxratovs279@gmail.com>
2026-07-23 10:00:47 +0500

    fix(router): correct closing tag to HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <App />
           <ToastContainer position="top-right" autoClose={3000} />
         </LanguageProvider>
-      </AuthProvider>
-    </BrowserRouter>
+        </AuthProvider>
+      </HashRouter>
   </React.StrictMode>
 )
